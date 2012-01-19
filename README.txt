48769bf2dfd1c779c948590590e24a994ab48823
Izak Burger <isburger@gmail.com>
2012-01-19 22:58:53 +0200

    initial checkin

---
+++ README.txt
@@ -0,0 +1,8 @@
+Introduction
+============
+
+slc.clicktracker injects tracking javascript into marked pages. That
+javascript then calls back so that we can track which users are viewing
+what pages. The basic use case for this is to make sure people in your
+organisation actually read a document before they vote against it in a
+meeting.
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='slc.clicktracker',
+    version=version,
+    description="Track user movements on your plone site.",
+    long_description=open("README.txt").read() + "\n" +
+                    open("HISTORY.txt").read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        ],
+    keywords='CMFEditions flexbox',
+    author='Izak Burger, Syslab.com GmbH',
+    author_email='isburger@gmail.com',
+    url='https://github.com/syslabcom/slc.clicktracker',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'' : 'src'},
+    namespace_packages=['slc'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlone',
+    ],
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins = ["ZopeSkel"],
+    )
