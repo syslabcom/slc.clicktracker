5bd1957f853e3cd0de595f55e90e576e8fb39301
Izak Burger <isburger@gmail.com>
2012-01-20 16:24:16 +0200

    oops, postgresql connections don't serialize, this breaks the zodb completely

--- src/slc/clicktracker/storage.py
+++ src/slc/clicktracker/storage.py
@@ -1,15 +1,13 @@
 import psycopg2
+from persistent import Persistent
 from zope.component import queryUtility
 from zope.interface import implements
 from plone.registry.interfaces import IRegistry
 from slc.clicktracker.interfaces import IClickStorage, IClickTrackerSettings
 
-class PostgresqlStorage(object):
+class PostgresqlStorage(Persistent):
 
     implements(IClickStorage)
-
-    def __init__(self):
-        self.connection = None
 
     def getSettings(self):
         registry = queryUtility(IRegistry)
@@ -17,28 +15,24 @@ class PostgresqlStorage(object):
             return None
         return registry.forInterface(IClickTrackerSettings)
 
-    def connect(self):
-        self.connection = None
-        try:
-            settings = self.getSettings()
-            if settings is not None and settings.dsn is not None:
-                self.connection = psycopg2.connect(settings.dsn)
-        except psycopg2.OperationalError:
-            pass
-
     @property
-    def connected(self):
-        if self.connection is None:
-            return False
-        return self.connection.closed == 0
+    def connection(self):
+        if hasattr(self, '_v_connection'):
+            if self._v_connection.closed == 0:
+                return self._v_connection
+        settings = self.getSettings()
+        if settings is not None and settings.dsn is not None:
+            try:
+                connection = psycopg2.connect(settings.dsn)
+            except psycopg2.OperationalError:
+                return None
+
+            self._v_connection = connection
+            return connection
 
     def logAccess(self, member, url):
         """ Log the given information, user visited url. """
-        if not self.connected:
-            self.connect()
-
-        if self.connected:
-            # TODO log to database
+        if self.connection is not None:
             cursor = self.connection.cursor()
             cursor.execute(
                 "SELECT logclick(%(member)s, %(document)s) AS result",
